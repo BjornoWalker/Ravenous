18e66cb31963de90a336f710a341d61b53ec7987
Bjorn Walker <walker_bjorn@yahoo.com>
2019-07-19 11:27:08 -0400

    Updates

---
+++ precache-manifest.be279a5dd1523b7cc651c43b602c0c04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51f6a92641101785a772347aaf9197e",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "65f340f413c232abe87c",
+    "url": "/Ravenous/static/css/main.6891c6b9.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/Ravenous/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "65f340f413c232abe87c",
+    "url": "/Ravenous/static/js/main.f1c7d0f0.chunk.js"
+  },
+  {
+    "revision": "5ddfa0b767d8254e6289",
+    "url": "/Ravenous/static/js/runtime~main.9078551a.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
